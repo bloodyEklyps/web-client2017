266b7ea1071785976cca1d9eaa491f6d90821027
AurélioH <bloody.eklyps@gmail.com>
2017-04-09 17:58:03 +0200

    final push?

--- src/scripts/getPhotos.js
+++ src/scripts/getPhotos.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 
-  //quand on click sur "b" ca affiche les photos
+  //au click sur un image
+  /*$("#photo_vue").click(function() {
+    //on ouvre la fenêtre modale
+    $("#dialog1").dialog("open");
+  });*/
+
+
+  //quand on click sur le bouton ca affiche les photos tout bien, tout en ordre
   $("#button").on("click", function(){
 
-    //ca marche pas vraiment, si le champ est vide ca fait pas la requete, mais si on met que des espaces ca le fait quand meme
+    //ca marche pas vraiment vu que si on met que des espaces ca fait la requete
+    //mais ca mrche si le champ est vide
     if($("#recherche").val().replace(" ", "") == ""){
-      console.log("pas content");
+      console.log("champ vide");
     } else {
       var url = 'http://api.flickr.com/services/feeds/photos_public.gne';
       var data = 'tagmode=any&format=json&tags='+ $("#recherche").val().replace(" ", "+");
@@ -18,44 +26,46 @@ $(document).ready(function(){
         dataType:'jsonp',
         jsonp: 'jsoncallback', // a renseigner d'après la doc du service, par défaut callback
 
-
-
         success:function(data){
-          console.log(data);
-
           //reset des images déja présentes
           $("#photo_vue").html("");
           $("#photo_table").html("");
 
           //data = objet avec json de toutes les photos
           //item = 1 objet de type photo
-          var html = "<thead><td>Image</td><td>Titre</td><td>Date</td><td>Description</td></thead>"
+          var html = "<thead><td>Image</td><td>Titre</td><td>Date</td><td>Description</td></thead>";
 
           $.each(data.items, function(i,item){
-            //console.log(item.media.m);
+
+            //on récupère toutes les données dont on a besoin
+            //depuis le json
             var date = item.date_taken;
             var titre = item.title;
             var desc = item.description;
             var img = "<img src="+item.media.m+"/>";
 
+            //on reformate la date pour les comparaison
             var tabDateHeure = date.split("T");
             var tabDate = tabDateHeure[0].split("-");
             var jour = parseInt(tabDate[2]);
             var mois = parseInt(tabDate[1]);
             var annee = parseInt(tabDate[0]);
 
+            //on récupère la date des formulaires
             var jourForm = $("#jour").val();
             var moisForm = $("#mois").val();
             var anneeForm = $("#annee").val();
 
-            console.log(moisForm);
-
-            //console.log("date=" + tabDate);
+            //pour le detail de la vue photo (fenetre modale)
+            var details = "<table><tr><td>Titre : "+titre+"</td><td>"+img+"</td><td>Prise le : "+jour+"/"+mois+"/"+annee+"</td>";
+            details +="<td>Description : </br>"+desc+"</td></table>";
 
             //si la date est trop vielle on ajoute pas
             if(annee > anneeForm){
               //ajout de la photo sur la vue photo
-              $("<img/>").attr("src", item.media.m).appendTo("#photo_vue");
+              $("<img/>").attr("src", item.media.m).attr("id", i).appendTo("#photo_vue");
+              $("<div>").attr("id", "dialog"+i).attr("title", "Détails").html(details).appendTo("#dialog");
+              $("#dialog"+i).dialog({autoOpen: false});
               $("</br>").appendTo("#photo_vue");
 
               //ajout de la photo sur la vue table
@@ -66,26 +76,10 @@ $(document).ready(function(){
 
             } else if(annee == anneeForm){
                 if(mois > moisForm){
-                  //ajout de la photo sur la vue photo
-                  $("<img/>").attr("src", item.media.m).appendTo("#photo_vue");
-                  $("</br>").appendTo("#photo_vue");
-
-                  //ajout de la photo sur la vue table
-                  html += "<td>"+img+"</td>";
-                  html += "<td>"+titre+"</td>";
-                  html += "<td>"+jour+"/"+mois+"/"+annee+"</td>";
-                  html += "<td>"+desc+"</td></tr>";
+                    //ajout photo ici
                 } else if(mois == moisForm){
                   if(jour >= jourForm){
-                    //ajout de la photo sur la vue photo
-                    $("<img/>").attr("src", item.media.m).appendTo("#photo_vue");
-                    $("</br>").appendTo("#photo_vue");
-
-                    //ajout de la photo sur la vue table
-                    html += "<td>"+img+"</td>";
-                    html += "<td>"+titre+"</td>";
-                    html += "<td>"+jour+"/"+mois+"/"+annee+"</td>";
-                    html += "<td>"+desc+"</td></tr>";
+                    //ajout photo ici
                   } else {
                     i--;
                   }//fin 1er else jour
@@ -110,8 +104,8 @@ $(document).ready(function(){
 
       });//fin du ajax
 
-    }//fin du else
+    }//fin du else (champ vide)
 
-  });
+  });//fin du onclick sur le bouton
 
-})
+});//fin document.ready
