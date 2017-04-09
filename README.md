617dc3ada650bdf49670772013a3c28dfc96eaa9
AurélioH <bloody.eklyps@gmail.com>
2017-04-09 16:21:37 +0200

    fonction avec la date

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ mini-projet de fin de s4
 fait :
   - le requete ajax pour l'api flickr
   - l'autocomplete
+  - vue table
+  - gérer les date dans la table
 
 à faire :
-  - vue table
+  - affichage du détail sur vue photo
--- src/scripts/getPhotos.js
+++ src/scripts/getPhotos.js
@@ -29,34 +29,79 @@ $(document).ready(function(){
 
           //data = objet avec json de toutes les photos
           //item = 1 objet de type photo
-          var html = $("#table").html();
+          var html = "<thead><td>Image</td><td>Titre</td><td>Date</td><td>Description</td></thead>"
 
           $.each(data.items, function(i,item){
             //console.log(item.media.m);
             var date = item.date_taken;
             var titre = item.title;
             var desc = item.description;
-            var img = "<img src="+item.media.m+"/>"
+            var img = "<img src="+item.media.m+"/>";
 
-            console.log("desc=" + desc);
+            var tabDateHeure = date.split("T");
+            var tabDate = tabDateHeure[0].split("-");
+            var jour = parseInt(tabDate[2]);
+            var mois = parseInt(tabDate[1]);
+            var annee = parseInt(tabDate[0]);
 
-            //ajout de la photo
-            $("<img/>").attr("src", item.media.m).appendTo("#photo_vue");
+            var jourForm = $("#jour").val();
+            var moisForm = $("#mois").val();
+            var anneeForm = $("#annee").val();
 
-            //html += "<tr><td>"+$("<img/>").attr("src", item.media.m).get(0)+"</td>";
-            html += "<td>"+img+"</td>";
-            html += "<td>"+titre+"</td>";
-            html += "<td>"+date+"</td>";
-            html += "<td>"+desc+"</td></tr>";
+            console.log(moisForm);
+
+            //console.log("date=" + tabDate);
+
+            //si la date est trop vielle on ajoute pas
+            if(annee > anneeForm){
+              //ajout de la photo sur la vue photo
+              $("<img/>").attr("src", item.media.m).appendTo("#photo_vue");
+              $("</br>").appendTo("#photo_vue");
+
+              //ajout de la photo sur la vue table
+              html += "<td>"+img+"</td>";
+              html += "<td>"+titre+"</td>";
+              html += "<td>"+jour+"/"+mois+"/"+annee+"</td>";
+              html += "<td>"+desc+"</td></tr>";
+
+            } else if(annee == anneeForm){
+                if(mois > moisForm){
+                  //ajout de la photo sur la vue photo
+                  $("<img/>").attr("src", item.media.m).appendTo("#photo_vue");
+                  $("</br>").appendTo("#photo_vue");
+
+                  //ajout de la photo sur la vue table
+                  html += "<td>"+img+"</td>";
+                  html += "<td>"+titre+"</td>";
+                  html += "<td>"+jour+"/"+mois+"/"+annee+"</td>";
+                  html += "<td>"+desc+"</td></tr>";
+                } else if(mois == moisForm){
+                  if(jour >= jourForm){
+                    //ajout de la photo sur la vue photo
+                    $("<img/>").attr("src", item.media.m).appendTo("#photo_vue");
+                    $("</br>").appendTo("#photo_vue");
+
+                    //ajout de la photo sur la vue table
+                    html += "<td>"+img+"</td>";
+                    html += "<td>"+titre+"</td>";
+                    html += "<td>"+jour+"/"+mois+"/"+annee+"</td>";
+                    html += "<td>"+desc+"</td></tr>";
+                  } else {
+                    i--;
+                  }//fin 1er else jour
+                } else {
+                  i--
+                }//fin 2eme else mois
+              } else {
+                i--;
+              } //fin 3eme else annee
 
             //la limite de photo, 20 max <-- changer avec le "spinner"
-            var nb = $("#spinner").val()
+            var nb = $("#spinner").val();
             if ( i == nb-1 ) return false ;
           });//fin du each
 
           $("#table").html(html);
-
-          console.log(html);
         },
 
         error: function(resultat,statut,erreur){
